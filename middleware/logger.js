021912392b6e202d064e9008273575e2bdaa1402
itisjustmeeee <guzairovaolga21@gmail.com>
2025-12-10 20:40:52 +0300

    feat: fixing error

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,5 @@
 function logger(req, res, next) {
-  console.log([\( {new Date().toISOString()}] \){req.method} ${req.originalUrl});
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
   next();
 }
 
